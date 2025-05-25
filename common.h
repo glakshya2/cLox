4cbf0b893d8ef936e298758a9161c173f3ca0cc3
glakshya2 <glakshya2@gmail.com>
2025-05-25 23:07:09 +0530

    A Virtual Machine

--- common.h
+++ common.h
@@ -5,8 +5,11 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#define DEBUG_TRACE_EXECUTION
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #endif //COMMON_H
---
+++ vm.h
@@ -0,0 +1,34 @@
+//
+// Created by glaks on 5/25/2025.
+//
+
+#ifndef VM_H
+#define VM_H
+
+#include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk* chunk;
+    uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stackTop;
+} VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
+
+void initVM();
+void freeVM();
+
+InterpretResult interpret(Chunk* chunk);
+
+void push(Value value);
+Value pop();
+
+#endif //VM_H
